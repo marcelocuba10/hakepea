22e9298597f58ccf5573016e12e097fe7cf5f585
marzelo1000 <marzelo1000@hotmail.com>
2020-06-09 14:58:01 -0400

    Push notificacion firebase adicionado en home page

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -3,6 +3,15 @@ import { LoadingController } from '@ionic/angular';
 import { AppService } from 'src/app/services/app.service';
 import { AngularFirestore } from '@angular/fire/firestore';
 
+import {
+  Plugins,
+  PushNotification,
+  PushNotificationToken,
+  PushNotificationActionPerformed
+} from '@capacitor/core';
+
+const { PushNotifications } = Plugins;
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
@@ -25,6 +34,48 @@ export class HomePage implements OnInit {
 
   ngOnInit() {
     this.getPost();
+
+    console.log('Initializing HomePage');
+
+    // Request permission to use push notifications
+    // iOS will prompt user and return if they granted permission or not
+    // Android will just grant without prompting
+    PushNotifications.requestPermission().then(result => {
+      if (result.granted) {
+        // Register with Apple / Google to receive push via APNS/FCM
+        PushNotifications.register();
+      } else {
+        // Show some error
+      }
+    });
+
+    PushNotifications.addListener('registration',
+      (token: PushNotificationToken) => {
+        //alert('Push registration success, token: ' + token.value);
+        console.log("Push registration success");
+      }
+    );
+
+    PushNotifications.addListener('registrationError',
+      (error: any) => {
+        //alert('Error on registration: ' + JSON.stringify(error));
+        console.log("Error on registration");
+      }
+    );
+
+    PushNotifications.addListener('pushNotificationReceived',
+      (notification: PushNotification) => {
+        //alert('Push received: ' + JSON.stringify(notification));
+        console.log("Push received");
+      }
+    );
+
+    PushNotifications.addListener('pushNotificationActionPerformed',
+      (notification: PushNotificationActionPerformed) => {
+        //alert('Push action performed: ' + JSON.stringify(notification));
+        console.log("Push action performed");
+      }
+    );
   }
 
   async getPost() {
